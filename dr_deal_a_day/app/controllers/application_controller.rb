19d861e00d136469700b150c66a3ce67b6ac1062
Brandon Burnett <gentoolicious@gmail.com>
2016-01-16 12:22:42 -0800

    Adding basic datafile import support.

--- dr_deal_a_day/app/controllers/application_controller.rb
+++ dr_deal_a_day/app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    @datafiles = DataFile.all
+  end
+
+  def upload
+    csv_file_path = params['file']
+    DataFile.import(csv_file_path)
+  end
 end
--- dr_deal_a_day/app/models/data_file.rb
+++ dr_deal_a_day/app/models/data_file.rb
@@ -1,2 +1,14 @@
 class DataFile < ActiveRecord::Base
+  def self.import(file)
+    require 'csv'
+
+    datafile = DataFile.find_or_create_by(file_name: file.original_filename, file_contents: file.tempfile.read)
+
+    CSV.foreach(file.path, headers: true, header_converters: :symbol) do |row|
+      purchaser = Purchaser.find_or_create_by(name: row[:purchaser_name])
+      merchant = Merchant.find_or_create_by(name: row[:merchant_name], address: row[:merchant_address])
+      item = Item.find_or_create_by(name: row[:item_description], price: row[:item_price])
+      order = Order.find_or_create_by(data_file_id: datafile.id, merchant_id: merchant.id, item_id: item.id, purchaser_id: purchaser.id, quantity: row[:purchase_count])
+    end
+  end
 end
